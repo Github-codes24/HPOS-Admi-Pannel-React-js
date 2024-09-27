abf87f51b6fec4db4a168f74058381cc93f9fe3a
vaishnawi <vaishudighore.9@gmail.com>
2024-09-27 11:39:27 +0530

    feat: code refactored

--- src/routes/private.routes.jsx
+++
@@ -1,3 +0,0 @@
-import Home from '../admin-components/admin-dashboard/Home';
-
-export const privateRoutes = [{ path: '/', element: <Home /> }];
--- src/routes/public.routes.jsx
+++
@@ -1,4 +0,0 @@
-import AdminSignIn from '../admin-components/AdminSignIn';
-
-
-export const publicRoutes = [{ path: '/adminsignin', element: <AdminSignIn /> }];
